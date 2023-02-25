4d0abb74fd8a6b45ad995a8e381671a6733ab302
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-02-25 23:07:27 +0400

    hello

---
+++ task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿int alisveris_meblegi = 2000;
+int endirim1 = (alisveris_meblegi * 95) / 100;
+int endirim2 = (alisveris_meblegi * 92) / 100;
+int endirim3 = (alisveris_meblegi * 90) / 100;
+if (alisveris_meblegi < 100)
+{
+    Console.WriteLine(endirim1);
+}
+else if (alisveris_meblegi >= 100 && alisveris_meblegi <= 1000)
+{
+    Console.WriteLine(endirim2);
+}
+else
+{
+    Console.WriteLine(endirim3);
+}
